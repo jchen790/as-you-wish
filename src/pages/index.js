ac79d04497435e909862415fc85863485723941a
jchen790 <jchen790@gmail.com>
2020-09-03 02:02:05 -0700

    setting up base site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,49 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const App = () => {
+  // Using set state to manage the chosen decision
+  const [chosenDecision, setChosenDecision] = useState("");
+  const [activities, setActivities] = useState([]);
+  const [newActivity, setNewActivity] = useState("");
+  const chooseActivity = () => {
+    setChosenDecision(activities[Math.floor(Math.random() * activities.length)]);
+  }
+  const addActivity = (e) => {
+    e && e.preventDefault();
+    setActivities([...activities, newActivity]);
+  }
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  // Using set state to adjust views for adding activities
+  const [showAddActivity, setShowAddActivity] = useState(false);
+
+  return (
+    <div className="root">
+      <div className="g_padding g_font chosen-decision">{chosenDecision}</div>
+      <div className="g_padding decision-buttons">
+        <button className="g_font-family g_font-size-regular g_border-radius g_button" onClick={chooseActivity}>Choose an activity</button>
+      </div>
+      <div className="g_width-full g_padding add-activity">
+        <button className="g_width-full g_font-family g_font-size-large add-activity-button" onClick={() => setShowAddActivity(!showAddActivity)}>
+          <div className="separator">Add activity</div>
+        </button>
+        {
+          showAddActivity &&
+          <form onSubmit={addActivity}>
+            <label className="g_font-family">
+              What activity would you like to add?
+              <br />
+              <input className="g_font-family g_font-size-regular g_border-radius g_input" type="text" value={newActivity} onChange={e => setNewActivity(e.target.value)} />
+            </label>
+            <input className="g_font-family g_font-size-regular g_border-radius g_button" type="submit" value="Add" />
+          </form>
+        }
+      </div>
+      <div className="g_padding g_font-family decorative-quote">
+        "Existence was really very simple when you did what you were told."
+        - The Princess Bride, William Goldman
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  );
+};
 
-export default IndexPage
+export default App
